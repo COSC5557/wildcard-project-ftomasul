65a0ba46a22c10b0f6954ed110060e0603eb7ef0
Finn <fmartsula@gmail.com>
2024-04-23 10:10:28 -0600

    initial commit

---
+++ Wildcard.R
@@ -0,0 +1,8 @@
+# Finn Tomasula Martin
+# COSC-4557
+# Wildcard
+# This file contains the code for the wildcard exercise
+
+# Clear environment
+rm(list = ls())
+while (!is.null(dev.list())) dev.off()
